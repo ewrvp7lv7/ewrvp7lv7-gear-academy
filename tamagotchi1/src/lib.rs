d5599da0bd53b6dcab11e3feb55f8f742e0d4b62
ewrvp7lv7 <vcxsw23@gmail.com>
2023-07-01 17:03:02 +0700

    Homework Tamagotchi2

--- tamagotchi1/src/lib.rs
+++ tamagotchi1/src/lib.rs
@@ -1,81 +1,55 @@
+//! #3. Homework "Tamagotchi"
 #![no_std]
-//https://academy.gear.rs/inner/hello-world-advanced/more-advanced/
-use gstd::{msg, prelude::*, debug}; //ERROR
-use tamagotchi_io::InputMessages;
 
-static mut GREETING: Option<String> = None;
+use gstd::{exec, msg, debug, prelude::*};
+use tamagotchi_io::{Tamagotchi, TmgAction, TmgEvent};
 
-// #[derive(Encode, Decode, TypeInfo)]
-// pub enum InputMessages {
-//     SendHelloTo(ActorId),
-//     SendHelloReply,
-// }
-
-// #[no_mangle]
-// extern "C" fn handle() {
-//     msg::reply(String::from("Hello"), 0)
-//         .expect("Error in sending a reply message");
-// }
-//
-// #[no_mangle]
-// extern "C" fn init() {
-//     // let init_message: String = msg::load()
-//     //     .expect("Can't load init message");
-//     // debug!("Program was initialized with message {:?}",
-//     //     init_message);
-// }
+static mut STATE: Option<Tamagotchi> = None;
 
 #[no_mangle]
 extern "C" fn init() {
-    let greeting: String = msg::load()
-        .expect("Can't decode an init message");
-    debug!("Program was initialized with message {:?}",
-        greeting);
-    unsafe { GREETING = Some(greeting) };
-}
-
-//https://academy.gear.rs/inner/hello-world-advanced/more-advanced/
-#[no_mangle]
-extern "C" fn handle() {
-    //https://docs.gear.rs/gstd/msg/fn.load.html
-    let input_message: InputMessages = msg::load()
-        .expect("Error in loading InputMessages");
-
-    let greeting = unsafe {
-        GREETING
-            //.as_mut()
-             .as_ref()
-            .expect("The contract is not initialized")
+    let name: String = msg::load().expect("Error in loading name");
+    let date_of_birth = exec::block_timestamp();
+    unsafe {
+        STATE = Some(Tamagotchi {
+            name,
+            date_of_birth,
+        })
     };
 
-    match input_message {
-        InputMessages::SendHelloTo(account) => {
-            debug!("Message: SendHelloTo {:?}", account);
-            msg::send(account, greeting, 0)
-                .expect("Error in sending Hello message to account");
-        }
-        InputMessages::SendHelloReply => {
-            debug!("Message: SendHelloReply");
-            msg::reply(greeting, 0)
-                .expect("Error in sending reply");
-        }
-    }
+    msg::reply(String::from("Success!"), 0)
+        .expect("Failed to share initialization result");
+}
+
+// Good job! Commented in the PR. The main concern i that you have not implemented the handle() function.
+// I am sorry, but I don't see the handle() function that receives TmgAction::Name and TmgAction::Age. Also, we need to cover this function with tests.
+#[no_mangle]
+extern "C" fn handle() {
+    let action: TmgAction = msg::load().expect("Error in loading TmgAction");
+    let state: &Tamagotchi = unsafe { STATE.as_ref().expect("failed to get contract state") };
+
+    let event: TmgEvent = match action {
+        TmgAction::Age => {
+            let age_min = (exec::block_timestamp() - state.date_of_birth) / 60_000 as u64;
+            debug!("Message: Age in min: {}", age_min);
+            TmgEvent::Age(age_min)
+        },
+        TmgAction::Name => TmgEvent::Name(state.name.clone())
+    };
+
+    msg::reply(event, 0).expect("Failed to share TmgEvent");
 }
 
 #[no_mangle]
 extern "C" fn state() {
-    let greeting = unsafe {
-        GREETING
-            .as_ref()
-            .expect("The contract is not initialized")
-    };
-    msg::reply(greeting, 0).expect("Failed to share state");
+    let state: &Tamagotchi = unsafe { STATE.as_ref().expect("failed to get contract state") };
+    msg::reply(state, 0).expect("Failed to share state");
 }
 
-#[no_mangle]
 // It returns the hash of metadata.
 // .metahash is generating automatically
 // while you are using build.rs
+#[no_mangle]
 extern "C" fn metahash() {
     let metahash: [u8; 32] = include!("../.metahash");
     msg::reply(metahash, 0)
--- tamagotchi1/tests/hello_world_test.rs
+++ tamagotchi1/tests/hello_world_test.rs
@@ -1,58 +1,44 @@
-// use gtest::{Log, Program, System};
-//
-// #[test]
-// fn hello_test() {
-//     let sys = System::new();
-//     sys.init_logger();
-//     let program = Program::current(&sys);
-// //Note that we added sys.init_logger() to initialize
-// //printing logs into stdout,
-// //and we sent a message from the user with id 2 (id 2 transforms to ActorId equal to 0x020000…00 ).
-//     program.send(2, String::from("INIT MESSAGE"));
-//
-//     let expected_log = Log::builder()
-//         .dest(2)
-//         .payload(String::from("Hello"));
-//     assert!(res.contains(&expected_log));
-// }
-
-
 use gtest::{Log, Program, System};
-// use hello_world::InputMessages;
-use tamagotchi_io::InputMessages;
+use tamagotchi_io::{TmgAction, TmgEvent};
 
 #[test]
 fn hello_test() {
     let sys = System::new();
     sys.init_logger();
     let program = Program::current(&sys);
-    // The first message to the Program structure is always the initialization message even if the program does not have the init function.
-    //let res = program.send_bytes(2, String::from("Hello"));
-    let res = program.send(2, String::from("Hello"));
-    assert!(!res.main_failed());
-    assert!(res.log().is_empty());
 
-    // test `SendHelloTo`
-    let hello_recipient: u64 = 4;
-    let res = program.send(
-        2,
-        InputMessages::SendHelloTo(hello_recipient.into()),
-    );
+    let t_name = "7Tamagotchi42";
+
+    // The first message to the Program structure is always the initialization message even if the program does not have the init function.
+    let sender_id = 2;
+    let res = program.send(sender_id, String::from(t_name));
+    assert!(!res.main_failed());
+    assert!(!res.log().is_empty());
+
+    //test init()
+    let expected_log = Log::builder()
+        .dest(sender_id)
+        .payload(String::from("Success!"));
+
+    assert!(res.contains(&expected_log));
+
+    // test for TmgAction::Name
+    let sender_id = 3;
+    let res = program.send(sender_id, TmgAction::Name);
 
     let expected_log = Log::builder()
-        .dest(hello_recipient)
-        .payload(String::from("Hello"));
+        .dest(sender_id)
+        .payload(TmgEvent::Name(String::from(t_name)));
 
-    assert!(res.contains(&expected_log))
+    assert!(res.contains(&expected_log));
+    assert!(!res.main_failed());
+
+    // test for TmgAction::Age
+    let sender_id = 3;
+    let res = program.send(sender_id, TmgAction::Age);
+
+    assert!(!res.main_failed());
 }
-
-
-// What is the purpose of the init() function in the smart contract code?
-// +To emulate the node behaviour for running programs
-// -To handle messages received by a program
-// -To create a new program
-// -To handle messages received by a program
-
 
 
 
