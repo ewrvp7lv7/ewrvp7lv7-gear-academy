8c5ef027e7d415d663d9d01d6abd6890335a88ed
ewrvp7lv7 <vcxsw23@gmail.com>
2023-07-02 20:00:12 +0700

    5. Homework Tamagotchi NFT Tests

--- tamagotchi.5/src/lib.rs
+++ tamagotchi.5/src/lib.rs
@@ -9,48 +9,6 @@ use nftt::NFTamagotchi;
 static mut STATE: Option<Tamagotchi> = None;
 static mut MOOD: Option<Mood> = None;
 
-// pub trait NFTamagotchi {
-//     fn transfer(&mut self, actor_id: ActorId);
-//     fn approve(&mut self, actor_id: ActorId);
-//     fn revoke_approval(&mut self);
-// }
-//
-// impl NFTamagotchi for Tamagotchi {
-//     fn transfer(&mut self, actor_id: ActorId) {
-//         let sender = msg::source();
-//         assert!(
-//             sender == self.owner || self.allowed_account == Some(sender),
-//             "Only owner or allowed account can transfer ownership"
-//         );
-//         self.owner = actor_id;
-//
-//         msg::reply(
-//             TmgEvent::Transfer(actor_id),
-//             0,
-//         ).expect("Failed to share the TmgEvent");
-//     }
-//
-//     fn approve(&mut self, actor_id: ActorId) {
-//         assert!(msg::source() == self.owner, "Only owner can approve");
-//         self.allowed_account = Some(actor_id);
-//
-//         msg::reply(
-//             TmgEvent::Approve(actor_id),
-//             0,
-//         ).expect("Failed to share the TmgEvent");
-//     }
-//
-//     fn revoke_approval(&mut self) {
-//         assert!(msg::source() == self.owner, "Only owner can revoke approval");
-//         self.allowed_account = None;
-//
-//         msg::reply(
-//             TmgEvent::RevokeApproval,
-//             0,
-//         ).expect("Failed to share the TmgEvent");
-//     }
-// }
-
 //Mood: Fed (from 1 to 10000), Happy (from 1 to 10000) and Rested (from 1 to 10000).
 pub struct Mood {
     pub fed: u32,
